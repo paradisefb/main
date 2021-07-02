fdffaeaca570e07dfaa5a9b8d230e6b5504e3234
paradisefb <86596984+paradisefb@users.noreply.github.com>
2021-07-02 19:02:56 +0800

    Update script.js

--- resources/JS/script.js
+++ resources/JS/script.js
@@ -27,22 +27,6 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
-}
-
-
 function copy() {
   var name, bank, acc, tel, feedback, reg, mutate, choose;
   name = document.getElementById("name").value;
